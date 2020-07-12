caf14b9d95a4eadbb1c90060a99f9f268c36dc4c
rmeason <rme15a@acu.edu>
2020-07-11 23:01:14 -0500

    added home, about, portfolio, and contact pages

---
+++ client/build/precache-manifest.e2620b3f6fbad6042b9301fb15754f1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b5e6b6738b74fbf64d",
+    "url": "/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "b8b5e6b6738b74fbf64d",
+    "url": "/static/js/main.87838918.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "88a9aaac280d6e8458718f71d266de84",
+    "url": "/index.html"
+  }
+];
