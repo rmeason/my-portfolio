3d228b1ae7c6bb0fab5f9826802422f87d1ea9e6
rmeason <rme15a@acu.edu>
2020-07-27 18:48:34 -0500

    updated portfolio page

--- client/src/components/pages/Portfolio.js
+++ client/src/components/pages/Portfolio.js
@@ -1,92 +1,72 @@
 import React from 'react';
 import "./Portfolio.css";
-import ccs from "./img/ccs.png"
+import ccs from "./img/ccs.png";
+import ts from "./img/ts.png";
 
 const Portfolio = () => (
 
-  <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-  <div class="orbit-wrapper">
-    <ul class="orbit-container">
-      <li class="is-active orbit-slide">
-      <div class="work-feature-block row">
-          <div class="columns medium-7">
-            <img class="work-feature-block-image" src={ccs} />
-          </div>
-          <div class="columns medium-5">
-            <h2 class="work-feature-block-header">Comic Character Source</h2>
-            <p>For this collaborative project I was in charge of the framework and stylistic capability of this site.</p>
-            <h2>Check it out!</h2>
-            <ul>
-              <li><a href="https://acschmalenberger.github.io/Project1/" target="_blank">Comic Charicter Source</a></li>
-            </ul>
-          </div>
-        </div>
-      </li>
-      <li class="orbit-slide">
-        <div class="work-feature-block row">
-          <div class="columns medium-7">
-            <img class="work-feature-block-image" src="#"/>
-          </div>
-          <div class="columns medium-5">
-            <h2 class="work-feature-block-header">   </h2>
-            <p>    </p>
-            <h2>   </h2>
-            <ul>
-              {/* <li>   </li>
-              <li>   </li>
-              <li>   </li>
-              <li>   </li> */}
-            </ul>
-          </div>
-        </div>
-      </li>
-      <li class="orbit-slide">
-        <div class="work-feature-block row">
-          <div class="columns medium-7">
-            <img class="work-feature-block-image" src="#"/>
-          </div>
-          <div class="columns medium-5">
-            <h2 class="work-feature-block-header">    </h2>
-            <p>  </p>
-            <h2>   </h2>
-            <ul>
-              {/* <li>   </li>
-              <li>   </li>
-              <li>   </li>
-              <li>   </li> */}
-            </ul>
-          </div>
-        </div>
-      </li>
-      <li class="orbit-slide">
-        <div class="work-feature-block row">
-          <div class="columns medium-7">
-            <img class="work-feature-block-image" src="#"/>
-          </div>
-          <div class="columns medium-5">
-            <h2 class="work-feature-block-header">    </h2>
-            <p>   </p>
-            <h2>   </h2>
-            <ul>
-              {/* <li>   </li>
-              <li>   </li>
-              <li>   </li>
-              <li>   </li> */}
-            </ul>
-          </div>
-        </div>
-      </li>
-    </ul>
+<div class="featured-image-block-grid">
+  <div class="featured-image-block-grid-header small-10 medium-8 large-7 columns text-center">
+    <h2>A list of my projects:</h2>
+    <p>click on the image to check out the sites</p>
   </div>
-  <nav class="orbit-bullets">
-    <button class="is-active" data-slide="0">
-      <span class="show-for-sr">First slide details.</span>
-      <span class="show-for-sr" data-slide-active-label>Current Slide</span>
-    </button>
-    <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-    <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-    <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-  </nav>
+  <div class="row large-up-4 small-up-2">
+    <div class="featured-image-block column">
+     <a href="https://acschmalenberger.github.io/Project1/" target="_blank">
+        <img src={ccs} />
+        <p class="text-center featured-image-block-title">Comic Character Source</p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="https://therapist-specialty.herokuapp.com/" target="_blank">
+        <img src={ts} />
+        <p class="text-center featured-image-block-title">Therarapist Specialty</p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="https://projectnumtwo.herokuapp.com/" target="_blank">
+        <img src= />
+        <p class="text-center featured-image-block-title">Cravings!</p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="https://vast-journey-67771.herokuapp.com/" target="_blank">
+        <img src="https://unsplash.it/600/440?image=792" />
+        <p class="text-center featured-image-block-title">Note App</p>
+      </a>
+    </div>
+    
+    {/* <div class="featured-image-block column">
+     <a href="#" target="_blank">
+        <img src="https://unsplash.it/600/440?image=667" />
+        <p class="text-center featured-image-block-title"> </p>
+      </a>
+    </div>
+  
+    <div class="featured-image-block column">
+     <a href="#" target="_blank">
+        <img src="https://unsplash.it/600/440?image=249" />
+        <p class="text-center featured-image-block-title"> </p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="#" target="_blank">
+        <img src="https://unsplash.it/600/440?image=382" />
+        <p class="text-center featured-image-block-title"> </p>
+      </a>
+    </div>
+
+    <div class="featured-image-block column">
+     <a href="#" target="_blank">
+        <img src="https://unsplash.it/600/440?image=1075" />
+        <p class="text-center featured-image-block-title"> </p>
+      </a>
+    </div> */}
+  </div>
 </div>
 
   );
